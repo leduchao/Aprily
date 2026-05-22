15cd5182bd38d5ab660a54229ba3bd6335c03c2c
Hao Le <leduchao77g1@gmail.com>
2026-05-22 18:58:56 +0700

    update result model

--- src/Aprily.SharedKernel/Result.cs
+++ src/Aprily.SharedKernel/Result.cs
@@ -2,30 +2,35 @@
 
 public class Result
 {
-    public bool IsSuccess { get; set; }
+    public bool IsSuccess { get; init; }
     public bool IsFailure => !IsSuccess;
 
-    public Error Error { get; set; } = default!;
+    public Error? Error { get; init; }
 
-    protected Result(bool isSuccess, Error error)
+    protected Result(bool isSuccess, Error? error = null)
     {
+        if (isSuccess && error is not null)
+        {
+            throw new ArgumentException("Invalid result", nameof(error));
+        }
+
         IsSuccess = isSuccess;
         Error = error;
     }
 
-    public static Result Success() => new(true, Error.None);
+    public static Result Success() => new(true);
     public static Result Failure(Error error) => new(false, error);
 }
 
 public class Result<TData> : Result
 {
-    public TData? Data { get; set; }
+    public TData? Data { get; init; }
 
-    private Result(bool isSuccess, TData? data, Error error) : base(isSuccess, error)
+    private Result(bool isSuccess, TData? data, Error? error = null) : base(isSuccess, error)
     {
         Data = data;
     }
 
-    public static Result<TData> Success(TData value) => new(true, value, Error.None);
+    public static Result<TData> Success(TData value) => new(true, value);
     public static new Result<TData> Failure(Error error) => new(false, default, error);
 }
